7602a1054619d4f61173547da7cc5597884124b0
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-11 16:46:14 -0400

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -289,7 +289,7 @@ void dfs(int v)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    for (int i = 0; i < pair_count - 1; i++)
+    for (int i = 0; i < pair_count; i++)
     {
         locked[pairs[i].winner][pairs[i].loser] = true;
     }
