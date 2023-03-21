9eaf9758fb84db48d6c11a9d778ae77bc3184a3e
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-21 19:58:42 -0400

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -128,24 +128,16 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int j = 0; j < candidate_count; j++)
     {
-        if (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name, candidates[j].name) == 0)
         {
-            preferences[voter][i] = rank;
+            preferences[voter][rank] = j;
 
+            // Vote recorded successfully
             return true;
         }
     }
-
-    /*for (int i = 0; i < voter_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            printf("%i ", preferences[i][j]);
-        }
-        printf("\n");
-    }*/
 
     return false;
 }
@@ -153,17 +145,7 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (!(candidates[i].eliminated))
-        {
-            for (int j = 0; j < voter_count; j++)
-            {
-                candidates[preferences[j][0]].votes++;
-            }
-        }
-    }
-
+    // TODO
     return;
 }
 
