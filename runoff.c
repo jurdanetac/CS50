3d3fef3d8b4d338ae753be43ae00bbf608799df3
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-23 19:14:48 -0400

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -183,7 +183,7 @@ bool print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= majority)
+        if (candidates[i].votes > majority)
         {
             printf("%s\n", candidates[i].name);
             return true;
