32d585cb3dd443e9a50998e65201deacfa342057
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-23 20:12:24 -0400

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -212,8 +212,17 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    bool tied = true;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!(candidates[i].votes == candidates[i + 1].votes))
+        {
+            return false
+        }
+    }
+
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
