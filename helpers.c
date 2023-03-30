b641628b9ca230a2b8c28f5de2d7cb93bf14b91b
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-30 17:11:43 -0400

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -3,13 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    BYTE average = 0;
+    int average = 0;
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+
             if (average % 2 != 0)
             {
                 average++;
