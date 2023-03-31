bc4d8ac2594e28b827ad86dda05297a6796cba9c
jurdanetac <jurdanetac@users.noreply.github.com>
2023-03-31 17:47:13 -0400

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -4,27 +4,35 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    double average = 0;
-    double decimal_part;
+    // Initialize needed variables
+    double average      = 0;
+    double decimal_part = 0;
 
+    // For each row of pixels
     for (int i = 0; i < height; i++)
     {
+        // For each pixel in row
         for (int j = 0; j < width; j++)
         {
+            // Calculate average of the three channels
             average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
+            // Calculate the floating point of the average, if any
             decimal_part = average - (int) average;
 
+            // Round if needed
             if (decimal_part >= 0.5)
             {
                 average++;
             }
 
+            // Set the new channels color for the pixel
             image[i][j].rgbtBlue  = (int) average;
             image[i][j].rgbtGreen = (int) average;
             image[i][j].rgbtRed   = (int) average;
         }
     }
 
+    // BMP converted successfully
     return;
 }
 
