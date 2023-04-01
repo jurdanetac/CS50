0d0a8ff9387f335258a6d3e283099a73e36ace05
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-01 13:03:57 -0400

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -58,9 +58,392 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// Checks whether the given color has decimal part, then it rounds accordingly
+void round_average(double* ptr)
+{
+    // e.g. 1.5 - 1 = 0.5
+    double decimal_part = *ptr - (int) *ptr;
+
+    if (decimal_part >= 0.5)
+    {
+        *ptr = (int) *ptr + 1;
+    }
+    else
+    {
+        *ptr = (int) *ptr;
+    }
+
+    return;
+}
+
+void fill_safe_image(int height, int width, RGBTRIPLE image[height][width],
+                     int safe_height, int safe_width, RGBTRIPLE safe_image[safe_height][safe_width])
+{
+    RGBTRIPLE placeholder;
+    placeholder.rgbtBlue  = 0;
+    placeholder.rgbtGreen = 0;
+    placeholder.rgbtRed   = 0;
+
+    // Fill safe matrix with just 0'ed bytes
+    for (int i = 0; i < safe_height; i++)
+    {
+        for (int j = 0; j < safe_width; j++)
+        {
+            safe_image[i][j] = placeholder;
+        }
+    }
+
+    // Copy original image which is now bordered by 0's
+    for (int i = 1; i < safe_height-1; i++)
+    {
+        for (int j = 1; j < safe_width-1; j++)
+        {
+            safe_image[i][j] = image[i-1][j-1];
+        }
+    }
+
+    return;
+}
+
+void copy_pixel_averages(int i, int j,
+                         int height, int width, RGBTRIPLE image[height][width],
+                         int safe_height, int safe_width, RGBTRIPLE safe_image[safe_height][safe_width])
+{
+    double average_blue  = 0;
+    double average_green = 0;
+    double average_red   = 0;
+
+    // if pixel is upper left corner
+    if (i == 1 && j == 1)
+    {
+        //
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i][j+1].rgbtRed;
+        average_red += safe_image[i+1][j].rgbtRed;
+        average_red += safe_image[i+1][j+1].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i][j+1].rgbtGreen;
+        average_green += safe_image[i+1][j].rgbtGreen;
+        average_green += safe_image[i+1][j+1].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i][j+1].rgbtBlue;
+        average_blue += safe_image[i+1][j].rgbtBlue;
+        average_blue += safe_image[i+1][j+1].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    // pixel is upper right corner
+    else if (i == 1 && j == width)
+    {
+        //
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i+1][j-1].rgbtRed;
+        average_red += safe_image[i+1][j].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i+1][j].rgbtGreen;
+        average_green += safe_image[i+1][j+1].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i][j+1].rgbtBlue;
+        average_blue += safe_image[i+1][j].rgbtBlue;
+        average_blue += safe_image[i+1][j+1].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    // pixel is lower left corner
+    else if (i == height && j == 1)
+    {
+        //
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i-1][j+1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i][j+1].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i-1][j+1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i][j+1].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i-1][j+1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i][j+1].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    // pixel is lower right corner
+    else if (i == height && j == width)
+    {
+        //
+        average_red += safe_image[i-1][j-1].rgbtRed;
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j-1].rgbtGreen;
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j-1].rgbtBlue;
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i][j-1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    else if (j == 1)
+    {
+        //
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i-1][j+1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i][j+1].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i-1][j+1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i][j+1].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i-1][j+1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i][j+1].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    else if (j == width)
+    {
+        //
+        average_red += safe_image[i-1][j-1].rgbtRed;
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j-1].rgbtGreen;
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j-1].rgbtBlue;
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i][j-1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    else if (i == 1)
+    {
+        //
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i+1][j-1].rgbtRed;
+        average_red += safe_image[i+1][j].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i+1][j-1].rgbtGreen;
+        average_green += safe_image[i+1][j].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i][j-1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i+1][j-1].rgbtBlue;
+        average_blue += safe_image[i+1][j].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    else if (i == height)
+    {
+        //
+        average_red += safe_image[i-1][j-1].rgbtRed;
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red /= 4;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j-1].rgbtGreen;
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green /= 4;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j-1].rgbtBlue;
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i][j-1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue /= 4;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+    else
+    {
+        //
+        average_red += safe_image[i-1][j-1].rgbtRed;
+        average_red += safe_image[i-1][j].rgbtRed;
+        average_red += safe_image[i-1][j+1].rgbtRed;
+        average_red += safe_image[i][j-1].rgbtRed;
+        average_red += safe_image[i][j].rgbtRed;
+        average_red += safe_image[i][j+1].rgbtRed;
+        average_red += safe_image[i+1][j-1].rgbtRed;
+        average_red += safe_image[i+1][j].rgbtRed;
+        average_red += safe_image[i+1][j+1].rgbtRed;
+        average_red /= 9;
+
+        round_average(&average_red);
+        image[i-1][j-1].rgbtRed = average_red;
+
+        //
+        average_green += safe_image[i-1][j-1].rgbtGreen;
+        average_green += safe_image[i-1][j].rgbtGreen;
+        average_green += safe_image[i-1][j+1].rgbtGreen;
+        average_green += safe_image[i][j-1].rgbtGreen;
+        average_green += safe_image[i][j].rgbtGreen;
+        average_green += safe_image[i][j+1].rgbtGreen;
+        average_green += safe_image[i+1][j-1].rgbtGreen;
+        average_green += safe_image[i+1][j].rgbtGreen;
+        average_green += safe_image[i+1][j+1].rgbtGreen;
+        average_green /= 9;
+
+        round_average(&average_green);
+        image[i-1][j-1].rgbtGreen = average_green;
+
+        //
+        average_blue += safe_image[i-1][j-1].rgbtBlue;
+        average_blue += safe_image[i-1][j].rgbtBlue;
+        average_blue += safe_image[i-1][j+1].rgbtBlue;
+        average_blue += safe_image[i][j-1].rgbtBlue;
+        average_blue += safe_image[i][j].rgbtBlue;
+        average_blue += safe_image[i][j+1].rgbtBlue;
+        average_blue += safe_image[i+1][j-1].rgbtBlue;
+        average_blue += safe_image[i+1][j].rgbtBlue;
+        average_blue += safe_image[i+1][j+1].rgbtBlue;
+        average_blue /= 9;
+
+        round_average(&average_blue);
+        image[i-1][j-1].rgbtBlue = average_blue;
+    }
+
+    return;
+}
+
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Technique for preventing accessing neighbors out of bounds safely
+    // using a bigger matrix taken from https://stackoverflow.com/a/652411
+    const int SAFE_HEIGHT = height + 2;
+    const int SAFE_WIDTH  = width + 2;
+    RGBTRIPLE safe_image[SAFE_HEIGHT][SAFE_WIDTH];
+
+    fill_safe_image(height, width, image, SAFE_HEIGHT, SAFE_WIDTH, safe_image);
+
+    // For each row of pixels
+    for (int i = 1; i <= height; i++)
+    {
+        // For each pixel in row
+        for (int j = 1; j <= width; j++)
+        {
+            copy_pixel_averages(i, j, height, width, image, SAFE_HEIGHT, SAFE_WIDTH, safe_image);
+        }
+    }
 
     // BMP converted successfully
     return;
