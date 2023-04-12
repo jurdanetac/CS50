8c1cd27586778bd915ca741c25f105251ed73de3
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-11 21:17:58 -0400

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -21,7 +21,7 @@ typedef struct node
 node;
 
 // Choose number of buckets in hash table
-const unsigned int N = LENGTH;
+const unsigned int N = LENGTH + 1;
 
 // Hash table
 node *table[N] = {NULL};
