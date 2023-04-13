b003217b1e5b4d22409df69d7347970430929136
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-13 15:10:10 -0400

    automated commit by check50 [check50=True]

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+    # Ask user for their name
+    name = input("What is your name?\n")
+    # Greet user
+    print(f"hello, {name}")
+
+
+# If running from this file
+if __name__ == "__main__":
+    # Code execution begins here
+    main()
