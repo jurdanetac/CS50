a814f6813f73abf31152abc0938fd74d54a1c069
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-18 17:12:39 -0400

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -1,11 +1,7 @@
-SELECT DISTINCT name
-FROM people
-JOIN stars AS s ON people.id = s.person_id
-JOIN movies AS m ON s.movie_id = m.id;
-WHERE title IN
-SELECT title
-FROM people
-JOIN stars AS s ON people.id = s.person_id
-JOIN movies AS m ON s.movie_id = m.id
-WHERE name = 'Kevin Bacon' AND birth = 1958
-AND name != 'Kevin Bacon' AND birth != 1958;
+SELECT name FROM people
+    WHERE id IN 
+    (SELECT person_id FROM stars
+        WHERE movie_id IN
+    (SELECT id FROM movies
+        WHERE name = 'Kevin Bacon' AND birth = 1958))
+AND name != 'Kevin Bacon';
