42972eb1a3d07b7570196ad1c3931a95cf6e14d8
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-18 15:40:06 -0400

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT name
 FROM people
-JOIN stars AS s ON people.id = s.person_id
-JOIN movies AS m ON s.movie_id = m.id
+JOIN directors AS d ON people.id = d.person_id
+JOIN movies AS m ON d.movie_id = m.id
 JOIN ratings as r ON m.id = r.movie_id
 WHERE rating >= 9.0;
