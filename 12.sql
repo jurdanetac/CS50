fc68c061175cb243724b8a2485337103a376cec5
jurdanetac <jurdanetac@users.noreply.github.com>
2023-04-18 16:04:05 -0400

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -2,4 +2,10 @@ SELECT title
 FROM people
 JOIN stars AS s ON people.id = s.person_id
 JOIN movies AS m ON s.movie_id = m.id
-WHERE name IN ('Johnny Depp', 'Helena Bonham Carter');
+WHERE name = 'Johnny Depp'
+INTERSECT
+SELECT title
+FROM people
+JOIN stars AS s ON people.id = s.person_id
+JOIN movies AS m ON s.movie_id = m.id
+WHERE name = 'Helena Bonham Carter';
